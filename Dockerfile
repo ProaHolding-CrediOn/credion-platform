0101fc89b3f9cd572e6c6ab2c4e8c144104f1a37
Jhon <jhongil96@gmail.com>
2025-07-04 22:20:48 -0500

    Se agregan los docker y las variables

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:20-alpine AS builder
+WORKDIR /app
+COPY package*.json ./
+COPY tsconfig*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM node:20-alpine AS runner
+WORKDIR /app
+ENV NODE_ENV=production
+COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/.next .next
+COPY --from=builder /app/public ./public
+COPY --from=builder /app/next.config.ts ./
+COPY --from=builder /app/node_modules ./node_modules
+
+# Si usás .env
+COPY --from=builder /app/.env .env
+
+EXPOSE 3001
+
+CMD ["npm", "start"]
--- hooks/useCountry.ts
+++ hooks/useCountry.ts
@@ -1,12 +1,33 @@
-export function useCountry() {
+import api from "@/lib/axiosInstance";
+import { useState } from "react";
 
-    const fetchCountries = async () => {
-        const res = await fetch('http://localhost:3000/api/countries?limit=0&sort=name');
-        const data = await res.json();
+export function useCountry() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    async function getCountries() {
+        const response = await api.get('countries?limit=0&sort=name');
+        const data = response.data;
         return data.docs;
     }
 
+    const fetchCountries = async () => {
+        try {
+            setLoading(true);
+            console.log("Obteniendo paises");
+            const response = await getCountries();
+            return response;
+        } catch (err) {
+            setError("Error obteniendo paises");
+            return { success: false, error: "Error al consultar paises" };
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return {
+        loading,
+        error,
         fetchCountries
     }
 
